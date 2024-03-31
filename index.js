33f09408a48ce05d8b76574dc3e3e22450cefbf2
Sarthak <sarthakrawatbiz@gmail.com>
2024-03-31 21:22:12 +0530

    exporting app

--- index.js
+++ index.js
@@ -50,4 +50,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = { factoryContract, marketplaceContract };
+module.exports = { factoryContract, marketplaceContract, app };
