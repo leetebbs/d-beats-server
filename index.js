771795f241dedc53f0327c9096b0c83ba77ed08c
Tebbo <leetebbs@ymail.com>
2024-03-31 15:54:01 +0100

    move files to root folder

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import routes from "./src/routes.js";
+import routes from "./routes.js";
 import { ethers } from "ethers";
-import factoryAbi from "./src/abi/factoryAbi.json" assert { type: "json" };
-import marketplaceAbi from "./src/abi/marketplaceAbi.json" assert { type: "json" };
+import factoryAbi from "./abi/factoryAbi.json" assert { type: "json" };
+import marketplaceAbi from "./abi/marketplaceAbi.json" assert { type: "json" };
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("api/", routes);
 app.use("/", routes);
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+ });
+ 
 
 const factoryAddress = "0x242309090561301ff7A27a1d675f2c0dA06845FD";
 const marketplaceAddress = "0x4690C5d846Abb49d0b6B2a04D4aa3B16e4aFC287";
@@ -34,9 +39,9 @@ export const marketplaceContract = new ethers.Contract(
 
 const PORT = process.env.REACT_APP_PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to D-Beat backend!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome to D-Beat backend!");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
