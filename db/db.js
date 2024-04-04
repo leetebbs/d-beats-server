894e02dce287d8b78ae211d7f5e7447d8d7cd6fc
Sarthak <sarthakrawatbiz@gmail.com>
2024-04-04 12:19:44 +0530

    databse changes

--- db/db.js
+++ db/db.js
@@ -1,27 +1,28 @@
 const WeaveDB = require("weavedb-sdk-node");
-require('dotenv').config(); // Correctly load environment variables
+require("dotenv").config(); // Correctly load environment variables
 
 const wallet = {
- getAddressString: () => process.env.ADMIN_ADDRESS.toLowerCase(),
- getPrivateKey: () => Buffer.from(process.env.ADMIN_PRIVATE_KEY, "hex"),
+  getAddressString: () => process.env.REACT_APP_ADMIN_ADDRESS.toLowerCase(),
+  getPrivateKey: () =>
+    Buffer.from(process.env.REACT_APP_ADMIN_PRIVATE_KEY, "hex"),
 };
 
 let db; // Define db outside of the init function
-const ownerAddress = process.env.ADMIN_ADDRESS; // Replace with the actual owner's address
+const ownerAddress = process.env.REACT_APP_ADMIN_ADDRESS; // Replace with the actual owner's address
 
 // Set the owner as a global variable
 global.owner = ownerAddress;
 // Initialize WeaveDB
 async function init() {
- db = new WeaveDB({ contractTxId: process.env.CONTRACT_TX_ID });
- await db.initializeWithoutWallet();
- db.setDefaultWallet(wallet, "evm");
+  db = new WeaveDB({ contractTxId: process.env.REACT_APP_CONTRACT_TX_ID });
+  await db.initializeWithoutWallet();
+  db.setDefaultWallet(wallet, "evm");
 }
 
 init();
 
 class DBManager {
- static async setArtistApplicationData(userId, twitterHandle) {
+  static async setArtistApplicationData(userId, twitterHandle) {
     try {
       await db.upsert(
         {
@@ -35,9 +36,43 @@ class DBManager {
     } catch (error) {
       throw new Error(`Error submitting artist application: ${error.message}`);
     }
- }
+  }
 
- // Other methods...
+  static async createDocument(collectionName, data) {
+    try {
+      await db.add(data, collectionName);
+    } catch (error) {
+      throw new Error(`Error creating document in WeaveDB: ${error.message}`);
+    }
+  }
+
+  static async queryDocuments(collectionName, query) {
+    try {
+      const documents = await db.get(collectionName, query);
+      return documents;
+    } catch (error) {
+      throw new Error(`Error querying documents in WeaveDB: ${error.message}`);
+    }
+  }
+
+  static async storeMusic(music) {
+    try {
+      await db.add(music, "Music");
+    } catch (error) {
+      throw new Error(`Error storing music in WeaveDB: ${error.message}`);
+    }
+  }
+
+  static async getMusicByArtist(artistId) {
+    try {
+      const music = await db.get("Music", ["artistId", "==", artistId]);
+      return music;
+    } catch (error) {
+      throw new Error(
+        `Error fetching music by artist in WeaveDB: ${error.message}`
+      );
+    }
+  }
 }
 
 module.exports = { DBManager, db };
