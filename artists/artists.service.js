e06ef410c2e58a87f58282f379f5edb2933af620
Sarthak <sarthakrawatbiz@gmail.com>
2024-04-05 23:55:53 +0530

    Created listed, cancelled and bought endpoint to fetch nft data

--- artists/artists.service.js
+++ artists/artists.service.js
@@ -4,33 +4,33 @@ const artistService = {
   async submitArtistApplication(userId, twitterHandle) {
     const wallet = "0x1ABc133C222a185fEde2664388F08ca12C208F76";
     const colls = 2;
-   const nuSales = 0;
-   const userIdd = 1;
+    const nuSales = 0;
+    const userIdd = 1;
     const twitter = "LTebbs2";
     const Verif = true;
 
-     try {
+    try {
       //  const db = await initDb();
-      console.log("trying")
-       const data = { wallet, colls, nuSales, userIdd, twitter, Verif };
-       // Assuming 'artists' is the collection name and 'userId' is the document ID
-       const tx = await db.add(data,"Artist");// this where you change the collection name
-       if (tx) {
-         console.log("success");
-          return tx;
-       } else {
-         console.log("failed", tx);
-       }
-     } catch (error) {
-       console.error(error); // Log the original error
-       throw new Error(`Error submitting artist application: ${error.message}`);
-     }
+      console.log("trying");
+      const data = { wallet, colls, nuSales, userIdd, twitter, Verif };
+      // Assuming 'artists' is the collection name and 'userId' is the document ID
+      const tx = await db.add(data, "Artist"); // this where you change the collection name
+      if (tx) {
+        console.log("success");
+        return tx;
+      } else {
+        console.log("failed", tx);
+      }
+    } catch (error) {
+      console.error(error); // Log the original error
+      throw new Error(`Error submitting artist application: ${error.message}`);
+    }
   },
 
-  async getArtists(){
+  async getArtists() {
     const result = await db.get("Artist");
     return result;
-  }
+  },
   // const addTask = async (task) => {
   //   await db.add(
   //     {
@@ -46,30 +46,31 @@ const artistService = {
   // };
   // Other methods remain unchanged
 
-
-// const artistService = {
-//  async submitArtistApplication(userId, twitterHandle, verificationStatus) {
-//     try {
-//       const data = { userId, twitterHandle ,verificationStatus:"pending"}; // Corrected key names
-//       // Assuming 'owner' is defined elsewhere or passed as an argument
-//       const tx = await db.setArtistApplicationData(data, "artists", owner);
-//       if (tx) {
-//         console.log("success");
-//       } else {
-//         console.log("failed",tx);
-//       }
-//     } catch (error) {
-//       console.error(error); // Log the original error
-//       throw new Error(`Error submitting artist application: ${error.message}`);
-//     }
-//   },
+  // const artistService = {
+  //  async submitArtistApplication(userId, twitterHandle, verificationStatus) {
+  //     try {
+  //       const data = { userId, twitterHandle ,verificationStatus:"pending"}; // Corrected key names
+  //       // Assuming 'owner' is defined elsewhere or passed as an argument
+  //       const tx = await db.setArtistApplicationData(data, "artists", owner);
+  //       if (tx) {
+  //         console.log("success");
+  //       } else {
+  //         console.log("failed",tx);
+  //       }
+  //     } catch (error) {
+  //       console.error(error); // Log the original error
+  //       throw new Error(`Error submitting artist application: ${error.message}`);
+  //     }
+  //   },
   async getPendingApplications() {
     try {
       // Fetch the list of pending artist applications from WeaveDB
       const pendingApplications = await db.getPendingArtistApplications();
       return pendingApplications;
     } catch (error) {
-      throw new Error(`Error fetching pending artist applications: ${error.message}`);
+      throw new Error(
+        `Error fetching pending artist applications: ${error.message}`
+      );
     }
   },
   async approveArtist(userId) {
@@ -79,7 +80,7 @@ const artistService = {
     } catch (error) {
       throw new Error(`Error approving artist: ${error.message}`);
     }
- },
+  },
 };
 
 module.exports = artistService;
--- index.js
+++ index.js
@@ -3,7 +3,616 @@ const cors = require("cors");
 const { ethers } = require("ethers");
 require("dotenv").config();
 var WeaveDB = require("weavedb-sdk-node");
-var nftAbi = require("./utils/nftAbi.js");
+const axios = require("axios");
+
+const nftAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "ERC721IncorrectOwner",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "ERC721InsufficientApproval",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "approver",
+        type: "address",
+      },
+    ],
+    name: "ERC721InvalidApprover",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+    ],
+    name: "ERC721InvalidOperator",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "ERC721InvalidOwner",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "receiver",
+        type: "address",
+      },
+    ],
+    name: "ERC721InvalidReceiver",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+    ],
+    name: "ERC721InvalidSender",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "ERC721NonexistentToken",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "OwnableInvalidOwner",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "OwnableUnauthorizedAccount",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "approved",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "bool",
+        name: "approved",
+        type: "bool",
+      },
+    ],
+    name: "ApprovalForAll",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "_fromTokenId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "_toTokenId",
+        type: "uint256",
+      },
+    ],
+    name: "BatchMetadataUpdate",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "_tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "MetadataUpdate",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "quantity",
+        type: "uint256",
+      },
+    ],
+    name: "mint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "safeTransferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes",
+        name: "data",
+        type: "bytes",
+      },
+    ],
+    name: "safeTransferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+      {
+        internalType: "bool",
+        name: "approved",
+        type: "bool",
+      },
+    ],
+    name: "setApprovalForAll",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "initialOwner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "artistAddress",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "_newTokenURI",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_mintAmount",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_symbol",
+        type: "string",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "_uri",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getApproved",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+    ],
+    name: "isApprovedForAll",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "ownerOf",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes4",
+        name: "interfaceId",
+        type: "bytes4",
+      },
+    ],
+    name: "supportsInterface",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "tokenURI",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
 
 //importing the abi
 const factoryAbi = require("./abi/factoryAbi.json");
@@ -26,26 +635,15 @@ async function init() {
 
 init();
 const wssProvider = new ethers.providers.WebSocketProvider(
-  `wss://arb-sepolia.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_ARB_SEPOLIA_KEY}`
+  `wss://arb-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_ARB_SEPOLIA_KEY}`
 );
 const provider = new ethers.providers.WebSocketProvider(
-  `https://arb-sepolia.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_ARB_SEPOLIA_KEY}`
+  `https://arb-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_ARB_SEPOLIA_KEY}`
 );
-//importing the routes
-// const routes = require("./routes.js");
-// const artistRoutes = require("./artists/artists.routes.js");
-// const uploadRoutes = require("./uploads/uploads.routes.js");
-// const nftsRoutes = require("./nfts/nfts.routes.js");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// Use the routes
-// app.use("/", routes);
-// app.use("/artists", artistRoutes);
-// app.use("/uploads", uploadRoutes);
-// app.use("/nfts", nftsRoutes);
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
@@ -61,17 +659,71 @@ app.get("/allNfts", async (req, res) => {
   const result = await db.get("nfts");
   res.status(200).json(result);
 });
-// endpoint to fetch all nfts listed
-app.get("/listed", async (req, res) => {
-  const result = await db.get("listed");
-  // console.log(result) 
-  // var contract = new ethers.Contract(
-  //   result.nftAddress,
-  //   nftAbi, 
-  //   provider
-  // )
 
-  // console.log("contract", contract);
+app.get("/listed", async (req, res) => {
+  try {
+    const result = await db.get("listed");
+    const formattedData = await Promise.all(
+      result.map(async (item) => {
+        const contract = new ethers.Contract(item.nftAddress, nftAbi, provider);
+        const name = await contract.name();
+        const tokenURI = await contract.tokenURI(item.tokenId);
+        const owner = await contract.ownerOf(item.tokenId);
+
+        // Fetch the image URL and music file URL from the token URI
+        const { imageURL, musicFileURL } = await fetchImageAndMusicURLs(
+          tokenURI
+        );
+
+        return {
+          nftAddress: item.nftAddress,
+          tokenId: item.tokenId,
+          price: item.price,
+          name: name,
+          imageURL: imageURL,
+          musicFileURL: musicFileURL, // Include the music file URL in the response
+          owner: owner,
+        };
+      })
+    );
+
+    res.status(200).json(formattedData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+async function fetchImageAndMusicURLs(tokenURI, retries = 0) {
+  try {
+    const httpTokenURI = tokenURI.replace(
+      "ipfs://",
+      "https://cloudflare-ipfs.com/ipfs/"
+    );
+    const metadataResponse = await axios.get(httpTokenURI);
+    const metadata = metadataResponse.data;
+    const imageURL = `https://cloudflare-ipfs.com/ipfs/${metadata.image.replace(
+      "ipfs://",
+      ""
+    )}`;
+    const musicFileURL = `${metadata.animation_url.replace(
+      "https://ipfs.io/",
+      "https://cloudflare-ipfs.com/"
+    )}`;
+    return { imageURL, musicFileURL };
+  } catch (error) {
+    throw error;
+  }
+}
+
+// endpoint to fetch all nfts cancelled
+app.get("/cancelled", async (req, res) => {
+  const result = await db.get("cancelled");
+  res.status(200).json(result);
+});
+
+// endpoint to fetch all nfts bought
+app.get("/bought", async (req, res) => {
+  const result = await db.get("bought");
   res.status(200).json(result);
 });
 
